6affa36b65fe0b31924ee3a12f0e1e64039a6755
Kristian Tan <kristian.tan@ntlworld.com>
2020-01-29 12:10:21 +0000

    Friendship values initialised randomly not based off opinion

--- Agent.py
+++ Agent.py
@@ -9,7 +9,6 @@ class Agent:
         self.friendship_values = {}
 
     def initialise_friendship_values(self, population):
-        # TODO: Refactor this to return a dict, and call it from the constructor
         """
         Set a friendship rating for all other agents.  Agents with similar opinions are more likely to have a higher friendship rating
         :param population: List
@@ -17,25 +16,8 @@ class Agent:
         """
         for agent in population:
             if agent is not self:
-                lower, upper = -1, 1
-                # Get the difference in opinion ratings
-                opinion_difference = abs(self.opinion_rating - agent.opinion_rating)
-                # Get a value, bigger difference in opinion = smaller friendship rating, and vice versa
-                friendship_value = abs(1 - opinion_difference)
-
-                # If agents are differing in opinion, negative friendship rating
-                if self.opinion_rating * agent.opinion_rating < 0:
-                    friendship_value *= -1
-
-                self.friendship_values[agent] = friendship_value
-
-    @staticmethod
-    def check_send_tweet():
-        """
-        Return true if this agent is going to tweet this time step
-        :return: boolean
-        """
-        return random.randint(0, 10) is 0
+                # TODO: Initialise friendship ratings randomly or from a seed i.e not based on opinion
+                self.friendship_values[agent] = random.uniform(-1, 1)
 
     def tweet_response(self, tweet):
         """
@@ -44,6 +26,13 @@ class Agent:
         :param tweet: Tweet
         :return: None
         """
-        opinion_difference = self.opinion_rating - tweet.opinion_rating
-        print(opinion_difference)
+        opinion_difference = abs(self.opinion_rating - tweet.opinion_rating)
+        friendship_rating = self.friendship_values.get(tweet.sender)
+
+        x = (self.opinion_rating + ((friendship_rating + 1) / 2) * tweet.opinion_rating) / 2
+        y = self.opinion_rating + (friendship_rating + 1) * opinion_difference
+        pass
+
+
+
 
