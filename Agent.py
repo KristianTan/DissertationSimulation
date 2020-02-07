81da4d7cb03c45d2e8b29cb58dc13fc8952ecafa
Kristian Tan <kristian.tan@ntlworld.com>
2020-02-07 09:22:09 +0000

    Method description for tweet response

--- Agent.py
+++ Agent.py
@@ -29,6 +29,17 @@ class Agent:
         opinion_difference = abs(self.opinion_rating - tweet.opinion_rating)
         friendship_rating = self.friendship_values.get(tweet.sender)
 
+        if self.opinion_rating * tweet.opinion_rating > 0:
+            """
+            Agents agree, so increase friendship rating and move opinion rating further the same direction 
+            based on friendship rating.  Higher friendship rating = greater affect on opinion
+            """
+        else:
+            """
+            Agents disagree, so decrease friendship rating and move opinion rating further in opposite direction to
+            the tweeter.
+            """
+
         x = (self.opinion_rating + ((friendship_rating + 1) / 2) * tweet.opinion_rating) / 2
         y = self.opinion_rating + (friendship_rating + 1) * opinion_difference
         pass
