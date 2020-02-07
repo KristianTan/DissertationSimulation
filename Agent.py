18ca4490407c6276197d35b5f8cb6186c39bdd4c
Kristian Tan <kristian.tan@ntlworld.com>
2020-02-07 10:47:47 +0000

    Added headers to columns of outputted csv's

--- Agent.py
+++ Agent.py
@@ -20,6 +20,14 @@ class Agent:
             if agent is not self:
                 self.friendship_values[agent.id] = random.uniform(-1, 1)
 
+        data = {'opinion_rating': self.opinion_rating}
+
+        for key in self.friendship_values:
+            data[key] = self.friendship_values[key]
+
+        data_frame = pandas.DataFrame(data=data, index=[0])
+        data_frame.to_csv('logs/' + str(self.id) + '.csv', header=True, mode='w', index=False)
+
     def tweet_response(self, tweet):
         """
         Update opinion rating based off opinion value of tweet and friendship value of sender.
@@ -29,7 +37,6 @@ class Agent:
         """
         opinion_difference = abs(self.opinion_rating - tweet.opinion_rating)
         friendship_rating = self.friendship_values.get(tweet.sender.id)
-
         x = (self.opinion_rating + ((friendship_rating + 1) / 2) * tweet.opinion_rating) / 2
         y = self.opinion_rating + (friendship_rating + 1) * opinion_difference
         pass
@@ -41,7 +48,7 @@ class Agent:
             data[key] = self.friendship_values[key]
 
         data_frame = pandas.DataFrame(data=data, index=[0])
-        data_frame.to_csv('logs/' + str(self.id) + '.csv', header=True, mode='a', index=False)
+        data_frame.to_csv('logs/' + str(self.id) + '.csv', header=False, mode='a', index=False)
         print(data_frame)
 
 
