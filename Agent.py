3fd57567e82e7336d9a2a5920e7e801ceb732aea
Kristian Tan <kristian.tan@ntlworld.com>
2020-02-27 12:41:36 +0000

    Used Ternary operator instead of if statements for multiplier decisions

--- Agent.py
+++ Agent.py
@@ -41,13 +41,10 @@ class Agent:
         """
         DEFAULT_MODIFIER = 0.01
         friendship_rating = self.friendship_values.get(tweet.sender.id)
-        opinion_multiplier = friendship_multiplier = 1
 
-        if (friendship_rating < 0 < tweet.opinion_rating) or (tweet.opinion_rating < 0 < friendship_rating):
-            opinion_multiplier = -1
-
-        if (tweet.opinion_rating < 0 < self.opinion_rating) or (self.opinion_rating < 0 < tweet.opinion_rating):
-            friendship_multiplier = -1
+        # Decide whether to increase or decrease values based on tweet values.
+        opinion_multiplier = -1 if (friendship_rating < 0 < tweet.opinion_rating) or (tweet.opinion_rating < 0 < friendship_rating) else 1
+        friendship_multiplier = -1 if (tweet.opinion_rating < 0 < self.opinion_rating) or (self.opinion_rating < 0 < tweet.opinion_rating) else 1
 
         opinion_modifier = DEFAULT_MODIFIER * opinion_multiplier
         friendship_modifier = DEFAULT_MODIFIER * friendship_multiplier
@@ -55,26 +52,6 @@ class Agent:
         updated_friendship_value = limit_values(friendship_rating + friendship_modifier)
         self.friendship_values.update({tweet.sender.id: updated_friendship_value})
         self.opinion_rating = limit_values(self.opinion_rating + opinion_modifier)
-
-        # # Alter opinion rating based off tweet
-        # # opinion_modifier = (tweet.opinion_rating / friendship_rating) / 100
-        # opinion_modifier = (friendship_rating / tweet.opinion_rating) / 100
-        # opinion_modifier = limit_values(opinion_modifier, upper=0.15, lower=-0.15)
-        # self.opinion_rating = limit_values(self.opinion_rating + opinion_modifier)
-        #
-        # # Alter friendship values based on opinion
-        # opinion_difference = abs(self.opinion_rating - tweet.opinion_rating)
-        # friendship_modifier = (1 - opinion_difference) / 100
-        # friendship_modifier = limit_values(friendship_modifier, upper=0.15, lower=-0.15)
-        #
-        # # If agents disagree, reduce friendship value instead of increasing
-        # if self.opinion_rating * tweet.opinion_rating < 0 < friendship_modifier:
-        #     friendship_modifier *= -1
-        #
-        # friendship_rating += friendship_modifier
-        #
-        # updated_friendship_rating = {tweet.sender.id: limit_values(friendship_rating)}
-        # self.friendship_values.update(updated_friendship_rating)
 
     def output_data(self):
         data = {'opinion_rating': self.opinion_rating}
